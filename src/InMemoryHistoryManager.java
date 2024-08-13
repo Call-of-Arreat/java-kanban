72fb222c2f95d2706c988c086658fb69dd820732
CallOfArreat <svetsokol@mail.ru>
2024-08-14 02:00:45 +0300

    ДЗ спринта 6. Самописный LinkedList добавлен. Актуализированы классы Задач и Менеджера задач

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int LIMIT_OF_HISTORY_LIST = 10;
     private final List<Task> taskInHistory;
+    private final CustomLinkedList history = new CustomLinkedList();
 
     public InMemoryHistoryManager() {
-
         this.taskInHistory = new ArrayList<>(LIMIT_OF_HISTORY_LIST);
     }
 
@@ -15,22 +17,84 @@ public class InMemoryHistoryManager implements HistoryManager {
         if (task == null) {
             return;
         }
-        if (taskInHistory.size() >= LIMIT_OF_HISTORY_LIST) {
-            taskInHistory.removeFirst();
-        }
-        taskInHistory.addLast(task);
+        remove(task.getId());
+        history.linkLast(task);
     }
 
 
     @Override
     public List<Task> getHistory() {
-        return taskInHistory;
+        return history.getTaskList();
     }
 
     @Override
     public void remove(int id) {
-
+        history.removeNode(history.innerMap.remove(id));
     }
 
+    static class Node {
+        private Node prev;
+        private final Task task;
+        private Node next;
 
+        private Node(Task task, Node prev, Node next) {
+            this.task = task;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private static class CustomLinkedList {
+        private final Map<Integer, Node> innerMap = new HashMap<>();
+        private Node head;
+        private Node tail;
+
+        private void linkLast(Task task) {
+            Node newNode = new Node(task, tail, null);
+
+            if (tail == null) {
+                head = newNode;
+            } else {
+                tail.next = newNode;
+            }
+            tail = newNode;
+            innerMap.put(task.getId(), newNode);
+        }
+
+        private void removeNode(Node node) {
+            if (node == null) {
+                return;
+            }
+            Node prev = node.prev;
+            Node next = node.next;
+
+            if (prev != null) {
+                prev.next = next;
+            }
+            if (next != null) {
+                next.prev = prev;
+            }
+
+            node.prev = null;
+            node.next = null;
+
+            if (node == head) {
+                head = next;
+            }
+            if (node == tail) {
+                tail = prev;
+            }
+        }
+
+        private List<Task> getTaskList() {
+            List<Task> list = new ArrayList<>();
+            Node elem = head;
+            while (elem != null) {
+                list.add(elem.task);
+                elem = elem.next;
+            }
+            return list;
+        }
+    }
 }
+
